4516d3ec8f7dc1f55f337c938d5fa93657e9bc9a
Zekeriya Koc <zekzekus@gmail.com>
2018-04-19 15:25:06 +0300

    protolude custom prelude added and configured

--- executable/Main.hs
+++ executable/Main.hs
@@ -1,6 +1,4 @@
--- It is generally a good idea to keep all your business logic in your library
--- and only use it in the executable. Doing so allows others to use what you
--- wrote in their libraries.
+import Protolude
 import qualified Seyler
 
 main :: IO ()
--- library/Seyler.hs
+++ library/Seyler.hs
@@ -1,6 +1,8 @@
 -- | An example module.
 module Seyler (main) where
 
+import Lib.Prelude
+
 -- | An example function.
 main :: IO ()
-main = putStrLn "Initial version!"
+main = putStrLn ("Initial version!" :: Text)
