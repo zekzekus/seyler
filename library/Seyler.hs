c2ac37a63b7891bf19b910c9163e552215d9d5e4
Zekeriya Koc <zekzekus@gmail.com>
2018-04-20 17:01:31 +0300

    initial data types to express basic url scheme commands

--- library/Seyler.hs
+++ library/Seyler.hs
@@ -1,8 +1,29 @@
--- | An example module.
-module Seyler (main) where
+module Seyler where
 
-import Lib.Prelude
+import           Lib.Prelude
 
--- | An example function.
+type Key = Text
+
+type Value = Text
+
+type Parameter = (Key, Value)
+
+type ParameterList = [Parameter]
+
+data Action
+    = Add           
+    | AddProject    
+    | Update        
+    | UpdateProject
+    | Show
+    | Search        
+    | Version       
+    deriving (Show)
+
+data Command = Command
+    { action     :: Action
+    , parameters :: ParameterList
+    } deriving (Show)
+
 main :: IO ()
 main = putStrLn ("Initial version!" :: Text)
